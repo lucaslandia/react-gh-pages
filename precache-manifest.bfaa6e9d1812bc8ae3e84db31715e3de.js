4369c279529316b9bf688e84a18ad63d5168bf0e
lucaslandia <lucaslandia@github>
2020-05-16 18:26:34 -0300

    Updates

---
+++ precache-manifest.bfaa6e9d1812bc8ae3e84db31715e3de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1527211b40e54783e0d1b74ce5c935d1",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "beaa5d5fce52386d8c91",
+    "url": "/react-gh-pages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "17c79084c1e20441ba34",
+    "url": "/react-gh-pages/static/js/2.a489c2db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gh-pages/static/js/2.a489c2db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "beaa5d5fce52386d8c91",
+    "url": "/react-gh-pages/static/js/main.21d2d30e.chunk.js"
+  },
+  {
+    "revision": "e7cbf49cba7e4a0e320f",
+    "url": "/react-gh-pages/static/js/runtime-main.efd8f609.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
